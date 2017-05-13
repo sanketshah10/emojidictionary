3d3cd83975ef9e5c1414951abd471a6ea1216543
sanket shah <sanketshah166@gmail.com>
2017-05-13 19:07:52 +0300

    new table view addition

--- emoji dictionary/ViewController.swift
+++ emoji dictionary/ViewController.swift
@@ -8,13 +8,38 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var emojitableview: UITableView!
+    
+    var emojis = ["😀", "🙂", "☹️", "😫", "😱"] // the emoji array
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        emojitableview.dataSource = self
+        emojitableview.delegate = self
+        
+        // this is required for table views
+        
+        self.view.backgroundColor = UIColor.whiteColor() // screen background color
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return emojis.count //rows are equal to total emojis in the emoji array
     }
 
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        
+        cell.textLabel?.text = emojis[indexPath.row]
+        
+        // this allocates the different emojis to specific index of array in the rows
+        return cell
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
